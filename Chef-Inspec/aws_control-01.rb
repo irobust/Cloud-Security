09b413e5f7e23f33096707e225cf8c303869133d
Phanupong Permpimol <phanupong@irobust.co.th>
2022-11-29 14:56:17 +0700

    update aws control

--- Chef-Inspec/aws_control-01.rb
+++ Chef-Inspec/aws_control-01.rb
@@ -2,4 +2,26 @@ describe aws_ec2_instances(aws_region: 'ap-southeast-1') do
     its('count') { should be <= 2 }
 end
 
+describe aws_ec2_instance('i-01a2349e94458a507') do
+    it { should exist }
+    it { should be_running }
+    it { should_not have_roles }y
+end
+
+aws_ec2_instances.where(tags: /"tier"=>"web"/).instance_ids.each do |id|
+    describe aws_ec2_instance(id) do
+        its('role') { should eq "test-role" }
+    end
+end 
+
+describe aws_ec2_instance(name: 'my-instance') do
+    it { should exist }
+end
+
+describe aws_ec2_instance('i-090c29e4f4c165b74') do
+    its('tags') { should include(key: 'Contact', value: 'Gilfoyle') }
+end
+
+
+
 
